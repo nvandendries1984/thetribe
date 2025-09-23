bec0b90a5bf33bb4a99203f42cdba62a528a164c
nvandendries1984 <niels@nvandendries.com>
2025-09-23 09:53:23 +0200

    Clear-out

--- server/src/modules/moderation/controller.js
+++
@@ -1,4 +0,0 @@
-// Moderation business logic (placeholder)
-export async function getModerationLogs({ guildId, userId }) {
-  // ...fetch logs from DB...
-}
--- server/src/modules/moderation/index.js
+++
@@ -1,8 +0,0 @@
-// Moderation module initializer
-import { registerModels } from './schema.js';
-import routes from './routes.js';
-
-export default function init(app, registerModel) {
-  registerModels(registerModel);
-  app.use('/api/moderation', routes);
-}
